520449e914c220c56d6683791177b6af5f321c73
ArthurRocumback <arthur.rocumback@hotmail.com>
2024-08-05 16:57:03 -0300

    Keys

---
+++ Seção7/8 Keys.js
@@ -0,0 +1,8 @@
+let key = {
+    /* Chave da propriedade */ 'chave 10' /* Valor da propriedade */: 10,
+    'chave 20' : 20,
+    'chave 30' : 30,
+}
+console.log(key);
+// Retorna qual a chave da propriedade
+console.log(Object.keys(key));
