9e8875a1be7b72a51958611b7fe2e2984e51e49f
ArthurRocumback <arthur.rocumback@hotmail.com>
2024-09-09 16:58:28 -0300

    Aula 13

--- Seção7/13 IndexOF&LastIndexOF.js
+++ Seção7/13 IndexOF&LastIndexOF.js
@@ -1,9 +1,8 @@
-let num = [2,1,2,2];
+let num = [1,22,30,4,45,1];
 
-console.log(num.indexOf(2));
+console.log(num.indexOf(1));
 
-console.log(num.lastIndexOf(2));
-
+console.log(num.lastIndexOf(1));
 
 
 // IndexOF Encontra o primeiro indice (+ o valor dentro de []) e o lastIndexOF faz o mosmo só que de trás para frente
